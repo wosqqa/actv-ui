f3e63db3c82c01d9e019ffa4a2a43382c3fb172b
chenzhenwei <chenzhenwei@021.com>
2022-08-24 18:41:31 +0800

    ant基础组件开发

---
+++ vant.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  name: 'actvUi',
+  build: {
+    css: {
+      preprocessor: 'less',
+    },
+    site: {
+      publicPath: '/actvUi/',
+    },
+  },
+  site: {
+    title: 'actvUi',
+    logo: 'https://fastly.jsdelivr.net/npm/@vant/assets/logo.png',
+    nav: [
+      {
+        title: '开发指南',
+        items: [
+          {
+            path: 'home',
+            title: '介绍',
+          },
+          {
+            path: 'quickstart',
+            title: '快速上手',
+          },
+        ],
+      },
+      {
+        title: '基础组件',
+        items: [
+          {
+            path: 'demo-button',
+            title: 'DemoButton 按钮',
+          },
+        ],
+      },
+    ],
+  },
+};
